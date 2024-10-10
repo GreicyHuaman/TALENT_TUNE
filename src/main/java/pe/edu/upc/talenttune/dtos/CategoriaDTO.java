8548f6144242c43f9f2a4d5ccfccb3453c7492aa
Greicy Huaman <greicyhuaman06@gmail.com>
2024-10-10 02:15:11 -0500

    Commit

--- src/main/java/pe/edu/upc/talenttune/dtos/CategoriaDTO.java
+++ src/main/java/pe/edu/upc/talenttune/dtos/CategoriaDTO.java
@@ -1,4 +1,22 @@
 package pe.edu.upc.talenttune.dtos;
 
 public class CategoriaDTO {
+    private int idCategoria;
+    private String tipoCategoria;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getTipoCategoria() {
+        return tipoCategoria;
+    }
+
+    public void setTipoCategoria(String tipoCategoria) {
+        this.tipoCategoria = tipoCategoria;
+    }
 }
