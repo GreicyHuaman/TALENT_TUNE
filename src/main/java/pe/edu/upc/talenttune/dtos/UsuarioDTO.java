de71e56fd76d347bf8ea1154159b4776bb878102
Greicy Huaman <greicyhuaman06@gmail.com>
2024-10-10 16:38:36 -0500

    Commit

--- src/main/java/pe/edu/upc/talenttune/dtos/UsuarioDTO.java
+++ src/main/java/pe/edu/upc/talenttune/dtos/UsuarioDTO.java
@@ -1,4 +1,105 @@
 package pe.edu.upc.talenttune.dtos;
 
+import java.time.LocalDate;
+
 public class UsuarioDTO {
+    private int idUsuario;
+    private String username;
+    private String password;
+    private String nombres;
+    private LocalDate fechaNacimiento;
+    private String descripcion;
+    private String pais;
+    private String agencia;
+    private String sexo;
+    private String estudios;
+    private Boolean enabled;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstudios() {
+        return estudios;
+    }
+
+    public void setEstudios(String estudios) {
+        this.estudios = estudios;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
 }
